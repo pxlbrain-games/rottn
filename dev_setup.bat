7d37bbe2c2fb0563650f4714613dec3a78cbb94e
Silas Bischoff <silas_bischoff@yahoo.de>
2018-05-20 03:49:42 +0200

    changed the dev env scripts to also work with pypy

--- dev_setup.bat
+++ dev_setup.bat
@@ -3,13 +3,13 @@ ECHO Only run this once!
 ECHO Create venv, add project path to it and install all dependencies?
 SET /p continue=[y/n] 
 IF %continue%==n EXIT
-ECHO Input path to Python 3.6+ installation
-SET /p path=(the directory that contains the python.exe, without a slash at the end): 
+ECHO Input path to Python 3.6+ Installation
+SET /p path=(Path to executable): 
 ECHO ##################################
 ECHO Creating Virtual Environment ...
 ECHO ##################################
 MKDIR %~dp0venv
-%path%\python.exe -m venv %~dp0venv
+%path% -m venv %~dp0venv
 ECHO Appending Development Directory to PYTHONPATH ...
 > %~dp0venv\Lib\site-packages\bossfight.pth ECHO %~dp0bossfight.client\
 >> %~dp0venv\Lib\site-packages\bossfight.pth ECHO %~dp0bossfight.core\
@@ -18,7 +18,11 @@ ECHO ##################################
 ECHO Installing Dependencies ...
 ECHO ##################################
 %~dp0venv\Scripts\python.exe -m pip install --upgrade pip
+%~dp0venv\Scripts\python.exe -m pip install --upgrade setuptools
 %~dp0venv\Scripts\python.exe -m pip install -r %~dp0requirements.txt
+%~dp0venv\Scripts\pypy3.exe -m pip install --upgrade pip
+%~dp0venv\Scripts\pypy3.exe -m pip install --upgrade setuptools
+%~dp0venv\Scripts\pypy3.exe -m pip install -r %~dp0requirements.txt
 ECHO ##################################
 ECHO Dev Setup completed.
 ECHO Use .\venv environment for testing
--- dev_update.bat
+++ dev_update.bat
@@ -3,7 +3,11 @@ ECHO ##################################
 ECHO Updating Dependencies ...
 ECHO ##################################
 %~dp0venv\Scripts\python.exe -m pip install --upgrade pip
-%~dp0venv\Scripts\python.exe -m pip install -r %~dp0requirements.txt
+%~dp0venv\Scripts\python.exe -m pip install --upgrade setuptools
+%~dp0venv\Scripts\python.exe -m pip install --upgrade -r %~dp0requirements.txt
+%~dp0venv\Scripts\pypy3.exe -m pip install --upgrade pip
+%~dp0venv\Scripts\pypy3.exe -m pip install --upgrade setuptools
+%~dp0venv\Scripts\pypy3.exe -m pip install --upgrade -r %~dp0requirements.txt
 ECHO ##################################
 ECHO Dev Update completed.
 ECHO ##################################
