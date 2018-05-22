b06bb88af41b90be694dd731b495729e47b861c2
Silas Bischoff <silas_bischoff@yahoo.de>
2018-05-22 23:52:07 +0200

    started ideas for controls

--- bossfight.client/bossfight/client/controls.py
+++ bossfight.client/bossfight/client/controls.py
@@ -3,3 +3,22 @@
 Provides mixins for cocos layers that grant access to player controls
 '''
 
+class Avatar:
+    '''
+    Mixin that makes a CocosNode a controllable character.
+    '''
+    pass
+
+class Clickable:
+    '''
+    Mixin that makes a CocosNode clickable and clicking on it
+    will call it's *on_click()* function.
+    '''
+    pass
+
+class Dragable:
+    '''
+    Mixin that makes a CocosNode dragable by mouse and calls
+    it's *on_drag()* and *on_drop()* function.
+    '''
+    pass
